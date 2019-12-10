197824f8696c7f8095289f0a75204adf963f48c1
Brian Douglass <corp@tps-eoltp.com>
2019-12-10 00:11:49 -0800

    --Enabled UserSecrets and now fetching the sub key from there.

---
+++ AzureMapsAspNet/Data/Configuration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureMapsAspNet.Data
+{
+    public class AzureMapsSubKey
+    {
+      public string subscriptionKey { get; set; }
+    }
+}
--- AzureMapsAspNet/Pages/Tutorials/interactiveSearch.cshtml.cs
+++ AzureMapsAspNet/Pages/Tutorials/interactiveSearch.cshtml.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using AzureMapsAspNet.Data;
+using Microsoft.Extensions.Configuration;
 
 namespace AzureMapsAspNet.Tutorials
 {
     public class interactiveSearchModel : PageModel
     {
+        private readonly IConfiguration _config;
+        public string _subscriptionKey = null;
+
+        public interactiveSearchModel(IConfiguration config)
+        {
+            _config = config;
+        }
         public void OnGet()
         {
+            var azureMapsConfig = _config.GetSection("AzureMapsSubKey")
+                .Get<AzureMapsSubKey>();
+            _subscriptionKey = azureMapsConfig.subscriptionKey;
+            ViewData["Message"] = $"My Azure Map Sub Key ={_subscriptionKey}";
+
+            ViewData["subKey"] = _subscriptionKey;
         }
     }
 }
